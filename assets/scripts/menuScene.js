eb1487c55816e467582f56d27140a0fc9ca55907
mistusaa <166435663+mistusaa@users.noreply.github.com>
2025-05-04 00:02:33 +0800

    Update menuScene.js

--- assets/scripts/menuScene.js
+++ assets/scripts/menuScene.js
@@ -12,15 +12,15 @@ class MenuScene extends Phaser.Scene {
     create() {
         this.add.image(0, 0, 'background').setOrigin(0);
 
-        this.add.image(750, 300, 'playBtn')
+        this.add.image(900, 300, 'playBtn')
             .setInteractive()
             .on('pointerdown', () => this.scene.start('GameScene'));
 
-        this.add.image(750, 450, 'creditsBtn')
+        this.add.image(900, 450, 'creditsBtn')
             .setInteractive()
             .on('pointerdown', () => this.scene.start('CreditsScene'));
 
-        this.add.image(750, 600, 'quitBtn')
+        this.add.image(900, 600, 'quitBtn')
             .setInteractive()
             .on('pointerdown', () => alert('You exited the game.'));
     }
